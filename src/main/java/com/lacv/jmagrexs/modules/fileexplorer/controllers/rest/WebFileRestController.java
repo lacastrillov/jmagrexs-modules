5a5b9bab0d3c4a18513aa4946491d8401d268441
e11001a <e11001a@LCOBGITSDMYZRN2.ena.us.experian.local>
2019-06-04 15:46:32 -0500

    Se agrega funcionalidad para mostrar imagenes thumbnail en explorador de archivos

--- src/main/java/com/lacv/jmagrexs/modules/fileexplorer/controllers/rest/WebFileRestController.java
+++ src/main/java/com/lacv/jmagrexs/modules/fileexplorer/controllers/rest/WebFileRestController.java
@@ -16,21 +16,33 @@ import com.lacv.jmagrexs.util.Util;
 import com.google.gson.Gson;
 import com.lacv.jmagrexs.modules.fileexplorer.constants.ExplorerConstants;
 import com.lacv.jmagrexs.modules.fileexplorer.model.dtos.WebFileDto;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.InputStream;
+import java.net.URI;
+import java.net.URL;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import javax.annotation.PostConstruct;
+import javax.imageio.ImageIO;
 import javax.servlet.http.HttpServletRequest;
+import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.io.IOUtils;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.server.ResponseStatusException;
 
 /**
  *
@@ -148,6 +160,49 @@ public class WebFileRestController extends RestEntityController {
 
         return result;
     }
+    
+    @RequestMapping(value = "/resizeimage/{maxWidth}/{maxHeight}.htm", method = {RequestMethod.GET})
+    @ResponseBody
+    public ResponseEntity<byte[]> resizeImage(@PathVariable int maxWidth, @PathVariable int maxHeight, @RequestParam String location) {
+        final HttpHeaders headers = new HttpHeaders();
+        try {
+            String extension = FilenameUtils.getExtension(location);
+            if (FileService.existsFileUrl(location)) {
+                System.out.println("imageExist= true");
+                String contentType = null;
+                if (extension.equalsIgnoreCase("png")) {
+                    contentType= MediaType.IMAGE_PNG_VALUE;
+                    headers.setContentType(MediaType.IMAGE_PNG);
+                } else if (extension.equalsIgnoreCase("gif")) {
+                    contentType= MediaType.IMAGE_GIF_VALUE;
+                    headers.setContentType(MediaType.IMAGE_GIF);
+                } else if (extension.equalsIgnoreCase("jpg") || extension.equalsIgnoreCase("jpeg")){
+                    contentType= MediaType.IMAGE_JPEG_VALUE;
+                    headers.setContentType(MediaType.IMAGE_JPEG);
+                }
+                
+                if(contentType!=null){
+                    URI uri = new URI(location.replace(" ", "%20"));
+                    URL url = uri.toURL();
+                    BufferedImage bufferedImage= ImageIO.read(url);
+
+                    //Obtener Imagen redimensionada
+                    BufferedImage resizeImage = FileService.resizeImage(bufferedImage, maxWidth, maxHeight);
+                    InputStream is = FileService.bufferedImageToInputStream(resizeImage, contentType);
+
+                    headers.setCacheControl("public, max-age=600000");
+                    ResponseEntity<byte[]> image = new ResponseEntity<>(IOUtils.toByteArray(is), headers, HttpStatus.CREATED);
+                    is.close();
+
+                    return image;
+                }
+            }
+        } catch (Exception ex) {
+            LOGGER.error("ERRROR getResizeImage", ex);
+        }
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Archivo no encontrado");
+    }
+
 
     @Override
     public String saveFilePart(int slice, String fieldName, String fileName, String fileType, int fileSize, InputStream filePart, Object idParent) {
